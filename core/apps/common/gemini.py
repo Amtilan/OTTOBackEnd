13b88499a55c7a75fa8e4b520fedfd098866bd8e
Amtilan <149930972+Amtilan@users.noreply.github.com>
2025-03-02 23:36:07 +0500

    get_better_pred_results

--- core/apps/common/gemini.py
+++ core/apps/common/gemini.py
@@ -56,7 +56,6 @@ class RecommendationGenerator:
         
     def _search_product_by_title(self, title: str) -> Optional[dict]:
         for product in self.product_catalog.get("products", []):  # Берем список продуктов
-            print(product)
             if product.get("title") == title:
                 return product
         return None
--- core/apps/pred_results/service.py
+++ core/apps/pred_results/service.py
@@ -10,7 +10,8 @@ from core.apps.customers.entities.customers import CustomerEntity
 from core.apps.pred_results.entity import Pred_resultsEntity
 from core.apps.pred_results.models import Pred_results
 from core.core.settings.main import env
-
+import requests
+from io import BytesIO
 @dataclass
 class BasePredResults:
     @abstractmethod
@@ -51,26 +52,32 @@ class ORMPredResults(BasePredResults):
     ) -> list[Pred_resultsEntity]:
         predres_dtos = Pred_results.objects.filter(customer_id=customer.id)
         return [predres_dto.to_entity() for predres_dto in predres_dtos]
-    def get_better_pred_results(self, image_file_path: str) -> dict[str, any]:
-        url = "https://api-us.faceplusplus.com/facepp/v1/skinanalyze"
-        data = {
+
+
+    def get_better_pred_results(image_file_path: str) -> dict[str, any]:
+        try:
+            with open(image_file_path, "rb") as f:
+                image_bytes = f.read()
+        except FileNotFoundError:
+            print(f"Файл {image_file_path} не найден. Проверьте путь.")
+            return None
+
+        files_skin = {"image_file": BytesIO(image_bytes)}
+
+        skin_url = "https://api-us.faceplusplus.com/facepp/v1/skinanalyze"
+
+        skin_data = {
             "api_key": env('API_key_FACE'),
             "api_secret": env('API_secret_FACE')
         }
-        files = None
-        if image_file_path:
-            try:
-                files = {"image_file": open(image_file_path, "rb")}
-            except FileNotFoundError:
-                print(f"Файл {image_file_path} не найден. Проверьте путь.")
-                return None
+
+        skin_response = None
+
         try:
-            response = requests.post(url, data=data, files=files)
-            response.raise_for_status()  
-            return response.json()  
+            skin_resp = requests.post(skin_url, data=skin_data, files=files_skin)
+            skin_resp.raise_for_status()
+            skin_response = skin_resp.json()
         except requests.exceptions.RequestException as e:
-            print(f"Ошибка запроса: {e}")
-            return None
-        finally:
-            if files and "image_file" in files:
-                files["image_file"].close()
+            print(f"Ошибка запроса для skin analyze: {e}")
+
+        return skin_response
